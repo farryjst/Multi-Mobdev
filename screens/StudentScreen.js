c83fca640e0697e60f1f39acc50e2b0520817844
unknown <farryvay69@gmail.com>
2019-11-07 17:38:57 +0700

    FlatList

--- screens/StudentScreen.js
+++ screens/StudentScreen.js
@@ -4,7 +4,7 @@ import { Text, View, FlatList, StyleSheet, Image } from 'react-native';
 const students = [
     { name: 'Bumi', imageUrl: 'https://cdn.pixabay.com/photo/2016/02/04/13/49/the-earth-1179212_960_720.png' },
     { name: 'Matahari', imageUrl: 'https://www.pinclipart.com/picdir/big/86-861633_big-image-gambar-matahari-dan-bintang-clipart.png' },
-    { name: 'Bulan', imageUrl: 'https://img2.pngdownload.id/20171220/zqq/moon-png-5a3ad93db91f66.39102419151380614175838479.jpg' }
+    { name: 'Bulan', imageUrl: 'https://pngimage.net/wp-content/uploads/2018/06/gambar-bulan-png-1.png' }
 ];
 
 const StudentScreen = () => {
