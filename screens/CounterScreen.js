4ec43b119b7915f5e78ff57bf65ee4c82a9dcc2a
eughenian <farryvay69@gmail.com>
2019-11-13 11:44:09 +0700

    update CounterScreen

--- screens/CounterScreen.js
+++ screens/CounterScreen.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { setLightEstimationEnabled } from 'expo/build/AR';
 
-class CounterScreen extends React.Component {
+export default class CounterScreen extends Component {
     constructor() {
         super();
         this.state = {
             counter: 0
-        }
+        };
     }
 
     render() {
         return (
             <View>
-                <Text>Counter Screen</Text>
+                <Text> Counter Screen</Text>
                 <Button
-                    title="Tambah"
+                    title="Increase"
                     onPress={() => this.setState({ counter: this.state.counter + 1 })}
                 />
                 <Button
-                    title="Kurang"
+                    title="Decrease"
                     onPress={() => this.setState({ counter: this.state.counter - 1 })}
                 />
-                <Text>Current Count: </Text>
+                <Text> Current counter: {this.state.counter}</Text>
             </View>
+
         );
     }
 }
 
-const styles = StyleSheet.create({});
 
-export default CounterScreen;
+const styles = StyleSheet.create({});
