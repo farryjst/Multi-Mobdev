6593987967acebcd7aba27791273020a6fc5a43d
eughenian <farryvay69@gmail.com>
2019-11-14 01:29:42 +0700

    update screen home/wisata

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Text, View, Button, ImageBackground, BackHandler } from 'react-native';
+import { Text, View, Button, ImageBackground, StyleSheet } from 'react-native';
 
-const HomeScreen = ({ navigation, BackHandler }) => {
+const HomeScreen = ({ navigation }) => {
     return (
         <ImageBackground source={require('../assets/homeBackground.png')} style={{ width: '100%', height: '100%' }}>
             <View>
@@ -18,11 +18,18 @@ const HomeScreen = ({ navigation, BackHandler }) => {
                 <Text></Text>
                 <Button
                     title="Exit"
-                    onPress={() => BackHandler.navigate('Exit')}
+                    onPress={() => navigation.navigate('Exit')}
                 />
             </View>
         </ImageBackground>
     );
 };
+
+const styles = StyleSheet.create({
+    ButtonStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    }
+});
 
 export default HomeScreen;
--- screens/WisataScreen.js
+++ screens/WisataScreen.js
@@ -3,9 +3,9 @@ import { Text, View, FlatList, StyleSheet, Image, ImageBackground } from 'react-
 import { AuthSession } from 'expo';
 
 const wisata = [
-    { name: 'Beach', imageUrl: '../assets/beach.jpg' },
-    { name: 'Forest', imageUrl: '../assets/forest.jpg' },
-    { name: 'Mountain', imageUrl: '../assets/mountain.jpg' }
+    { name: 'Pulisan', imageUrl: 'https://pesona.travel/media/serunya-bermain-air-di-pantai-pulisan-likupang_115800_1140.jpg' },
+    { name: 'Bunaken', imageUrl: 'https://static.limakaki.com/2017/10/bunaken-dive.jpg' },
+    { name: 'Danau Linow', imageUrl: 'https://www.indonesiakaya.com/uploads/_images_gallery/12__Duduk_santai_dan_memandangi_keindahan_Danau_Linow_memberikan_nuansa_ketenangan_tersendiri.jpg' }
 ];
 
 const WisataScreen = () => {
@@ -14,12 +14,13 @@ const WisataScreen = () => {
             <FlatList
                 data={wisata}
                 renderItem={({ item }) =>
-                    <View style={styles.textStyle}>
+                    <View style={styles.viewStyle}>
+                        <Text></Text>
                         <Image
-                            style={{ width: 200, height: 200 }}
+                            style={{ width: 250, height: 250 }}
                             source={{ uri: item.imageUrl }}
                         />
-                        <Text >
+                        <Text style={styles.textStyle} >
                             {item.name}
                         </Text>
                     </View>
@@ -34,7 +35,12 @@ const WisataScreen = () => {
 const styles = StyleSheet.create({
     textStyle: {
         margin: 'auto',
-        alignItems: 'center'
+        alignItems: 'center',
+        color: 'white'
+    },
+    viewStyle: {
+        margin: 'auto',
+        alignItems: 'center',
     }
 });
 
