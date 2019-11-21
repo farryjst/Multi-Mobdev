9163ba8a919b7bfd80234b6201884f71ea6b1abc
eughenian <farryvay69@gmail.com>
2019-11-21 15:34:21 +0700

    tambah

--- screens/GaleryScreen.js
+++ screens/GaleryScreen.js
@@ -16,11 +16,11 @@ export default class Galery extends Component {
         super(props);
         this.state = {
             data: [
-                { id: 1, likes: 12, image: "https://lorempixel.com/400/200/nature/6/" },
-                { id: 2, likes: 11, image: "https://lorempixel.com/400/200/nature/5/" },
-                { id: 3, likes: 25, image: "https://lorempixel.com/400/200/nature/4/" },
-                { id: 4, likes: 12, image: "https://lorempixel.com/400/200/nature/6/" },
-                { id: 5, likes: 10, image: "https://lorempixel.com/400/200/sports/1/" },
+                { id: 1, likes: 12, image: "https://rentalcarmanado.com/wp-content/uploads/2018/12/Manado-Senja.jpg" },
+                { id: 2, likes: 11, image: "https://allesgroen.files.wordpress.com/2017/05/pulau-manado-tua.jpg" },
+                { id: 3, likes: "29K", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQibnVbaUcYzmPWIb1iuFuUjR3dyjWBg4E-ecxthAUwGt15Wx8Q&s" },
+                { id: 4, likes: "33K", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqwyrVGCCp9FiACB5yvuGx5pzFeHoxHaS3_h09eAbunpHJNCs_Zg&s" },
+                { id: 5, likes: 10, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWqviDiwOtUUn2_xzGTlsuXpA9La_YNBPCjfx6CdLsuVRGcVdmrg&s" },
                 { id: 6, likes: 12, image: "https://lorempixel.com/400/200/nature/8/" },
                 { id: 7, likes: 34, image: "https://lorempixel.com/400/200/nature/1/" },
                 { id: 8, likes: 45, image: "https://lorempixel.com/400/200/nature/3/" },
